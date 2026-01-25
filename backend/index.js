8e3e5dc36e4f92a73f704d5392dd5ed409502c09
Archi Agrawal <archiagrawal387@gmail.com>
2026-01-25 20:08:18 +0530

    added app method

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const routes = require("./Routes/routes.js");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
